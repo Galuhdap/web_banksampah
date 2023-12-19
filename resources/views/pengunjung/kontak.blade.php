8f59144c774208995e441d8dc1be4548892f404b
Galuh DAP <90491250+Galuhdap@users.noreply.github.com>
2023-12-19 10:45:39 +0700

    update
    news branch

---
+++ resources/views/pengunjung/kontak.blade.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+
+<head>
+    <script src="../assets/js/color-modes.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bank Sampah Digital Mojo | Tentang Kami</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/jumbotrons/">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="tentangkami.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="container">
+        <header class="border-bottom lh-1 py-3">
+
+        </header>
+
+        <div class="nav-scroller py-1 mb-3 border-bottom">
+            <nav class="nav nav-underline justify-content-between">
+                <a class="nav-item nav-link link-body-emphasis" href="/">Beranda</a>
+                <a class="nav-item nav-link link-body-emphasis" href="/postingan">Postingan</a>
+                <a class="nav-item nav-link link-body-emphasis" href="/produk">Produk</a>
+                <a class="nav-item nav-link link-body-emphasis" href="/tentangkami">Tentang Kami</a>
+                <a class="nav-item nav-link link-body-emphasis" href="/kontak">Kontak</a>
+            </nav>
+        </div>
+    </div>
+
+
+    <div class="container my-5">
+        <div class="p-5 text-center bg-body-tertiary rounded-3">
+            <h1 class="text-body-emphasis">Contact Us</h1>
+            <h4 class="lead">
+                Untuk informasi lebih lanjut atau mungkin ada pertanyaan mengenai produk dari kami, bisa menghubungi
+                nomor dibawah ini atau mengunjungi tempat kami di alamat yang sudah tertera dibawah
+            </h4>
+        </div>
+    </div>
+    <section class="Contact mb-5 bg-body-tertiary rounded-3 ">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-4">
+                    <div class="mb-3 text-center">
+                        <div class="card-body">
+                            <h3 class="mt-3">Alamat :</h3>
+                            <div class="mt-2 card-text d-flex mb-2">
+                                <span class="fa-solid fa-location-dot mr-5"></span>
+                                Jl. Kalidami Jl. Jojoran IIIA. Mojo, Kec. Gubeng, Surabaya, Jawa Timur 60285.
+                            </div>
+                            {{-- <div class="mt-3 card-text d-flex">
+                                <span class="mr-4 fa-solid fa-envelope"></span>
+                                mojobanksampah@gmail.com
+                            </div> --}}
+                        </div>
+                    </div>
+
+                    <div class="text-center mt-4">
+                        <h3>Hubungi Kami :</h3>
+                        <span class="glyphicon glyphicon-envelope"></span>
+                        <ul class="list-group list-group-flush">
+                            <h4 class="mt-2">+62 853-7748-786</h4>
+                            <h4 class="mt-2">ppkormawauntag17@gmail.com</h4>
+                        </ul>
+
+                        <ul class="mt-4 list-inline">
+                            <a href="https://www.instagram.com/ppkormawauntag/"><i class="list-inline-item fab fa-instagram fa-2x"></i></a>
+                            <i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+                        </ul>
+
+                    </div>
+
+                </div>
+
+            </div>
+    </section>
+
+    <footer class="text-body-secondary py-5 justify-content-center">
+        <div class="container">
+            <p class="float-end mb-1">
+                <a href="/">Back to top</a>
+            </p>
+            <p class="mb-1">Bank Sampah Digital Mojo 2023</p>
+
+        </div>
+    </footer>
+
+    <div class="b-example-divider"></div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
---
+++ resources/views/pengunjung/tentangkami.blade.php
@@ -0,0 +1,230 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+
+<head>
+    <script src="../assets/js/color-modes.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bank Sampah Digital Mojo | Tentang Kami</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/jumbotrons/">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+
+    <!-- Custom styles for this template -->
+    <link href="tentangkami.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="container">
+        <header class="border-bottom lh-1 py-3">
+
+        </header>
+
+        <div class="nav-scroller py-1 mb-3 border-bottom">
+            <nav class="nav nav-underline justify-content-between">
+                <a class="nav-item nav-link link-body-emphasis" href="/">Beranda</a>
+                <a class="nav-item nav-link link-body-emphasis" href="/postingan">Postingan</a>
+                <a class="nav-item nav-link link-body-emphasis" href="/produk">Produk</a>
+                <a class="nav-item nav-link link-body-emphasis" href="/tentangkami">Tentang Kami</a>
+                <a class="nav-item nav-link link-body-emphasis" href="/kontak">Kontak</a>
+            </nav>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <div class="p-5 text-center bg-body-tertiary rounded-3">
+            {{-- <svg class="bi mt-4 mb-3" style="color: var(--bs-indigo);" width="100" height="100">
+                <use xlink:href="#bootstrap" />
+            </svg> --}}
+
+            <img src="/images/hero4.jpg" alt=""
+            class="bd-placeholder-img card-img-top" width="100%" height="500">
+
+        </div>
+
+    </div>
+
+    <div class="b-example-divider"></div>
+
+    <div class="b-example-divider"></div>
+
+    <div class="my-5">
+        <div class="p-5 text-center bg-body-tertiary">
+            <div class="container py-5">
+                <!-- <h1 class="text-body-emphasis">Full-width jumbotron</h1> -->
+                <div class="container">
+                    <div class="row mb-4 pd-4 ">
+                        <div class="col text-center">
+                            <h1>Tentang Kami</h1>
+                        </div>
+                    </div>
+                </div>
+                <section class="abaut">
+
+                    <div class="row justify-content-center">
+                        <div class="col-md-5 text-center">
+                            <p>Mojo adalah kelurahan yg paling rapih dan terkoordinir dari kelurahan di kecamatan gubeng
+                                ini mengenai pengolahan
+                                sampahnya. tapi tak heran jika masa ke masa perlu adanya sentuhan teknologi agar
+                                pendataan sampah tiap warga
+                                berjala dengan lancar. salah satunya adalah dibuatkannya aplikasi administrasi </p>
+                        </div>
+                        <div class="col-md-5 text-center">
+                            <p>hingga pembukuan
+                                bank sampah di kelurahan mojo. tak hanya itu, kami dari tim HIMATIFTA juga menciptakan
+                                sensor iOt pendeteksi
+                                pembludakan sampah yang terjadi di gudang bank sampah yang
+                                dimana bangunan tersebut adalah hasil dari perjuangan kita dengan masyarakat Mojo</p>
+                        </div>
+
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    <div class="b-example-divider"></div>
+
+    {{-- <div class="container my-5">
+        <div class="p-5 text-center bg-body-tertiary rounded-3">
+            <div class="container">
+                <div class="row mb-4 pd-4 ">
+                    <div class="col text-center">
+                        <h1>Toko Masyarakat</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140"
+                        xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140"
+                        preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777"
+                            dy=".3em">140x140</text>
+                    </svg>
+
+                    <h2>Nama</h2>
+                    <p>Keterangan</p>
+                    <!-- <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p> -->
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140"
+                        xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140"
+                        preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777"
+                            dy=".3em">140x140</text>
+                    </svg>
+
+                    <h2>Nama</h2>
+                    <p>Keterangan</p>
+                    <!-- <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p> -->
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140"
+                        xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140"
+                        preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777"
+                            dy=".3em">140x140</text>
+                    </svg>
+
+                    <h2>Nama</h2>
+                    <p>Keterangan</p>
+                    <!-- <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p> -->
+                </div><!-- /.col-lg-4 -->
+            </div><!-- /.row -->
+        </div>
+    </div> --}}
+    
+    
+    {{-- <div class="container marketing">
+        <!-- START THE FEATURETTES -->
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading">Prestasi</h2>
+                <p class="lead">Keterangan</p>
+            </div>
+            <div class="col-md-5">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+                    width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img"
+                    aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa"
+                        dy=".3em">500x500</text>
+                </svg>
+
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7 order-md-2">
+                <h2 class="featurette-heading">Prestasi </h2>
+                <p class="lead">Keterangan</p>
+            </div>
+            <div class="col-md-5 order-md-1">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+                    width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img"
+                    aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa"
+                        dy=".3em">500x500</text>
+                </svg>
+
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading">Prestasi</h2>
+                <p class="lead">Keterangan</p>
+            </div>
+            <div class="col-md-5">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+                    width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img"
+                    aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa"
+                        dy=".3em">500x500</text>
+                </svg>
+
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <!-- /END THE FEATURETTES -->
+
+    </div><!-- /.container --> --}}
+
+    <footer class="text-body-secondary py-5 justify-content-center">
+        <div class="container">
+            <p class="float-end mb-1">
+                <a href="/">Back to top</a>
+            </p>
+            <p class="mb-1">Bank Sampah Digital Mojo 2023</p>
+
+        </div>
+    </footer>
+
+    <div class="b-example-divider"></div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
